967297b017dbba7ad6bf9a409656bc7c8a462569
AvivaShooman <vavass18@gmail.com>
2020-01-05 21:55:19 -0500

    Code works!

--- Final_Project_new.c
+++ Final_Project_new.c
@@ -9,7 +9,7 @@ int main(void){
   char *filename2 = "./output2.txt";
   char readbuf[80];
   char *ans1, *ans2;
-  int pipe_test, number1, number2;
+  int pipe_test, number1, number2, Char1, Char2;
 
   /* Create one way pipeline in with popen() that reads in the input file */
   if ((pipe_read = popen("cat supermarket_sales.csv", "r")) == NULL){
@@ -36,10 +36,38 @@ int main(void){
   pclose(pipe_read);
   pclose(pipe_transform);
 
-  /* Check that the pipe implemented above has the same output*/
-  if ((pipe_test = system("cat supermarket_sales.csv | grep -i 'Fashion accessories' | cut -d ',' -f 10| awk '{s+=$1} END {printf \"%0.2f\", s}'> output2.txt")) != -1){
-    perror("system");
-    exit(1);
+  /* Test Code: Make sure both files contain the same value */
+
+  /* open output1.txt file for reading */
+  test_file1 = fopen(filename1, "r");
+  if (test_file1 == NULL) {
+    printf("Could not open input file %s\n", filename1);
+    perror(filename1);
+    return(1);
   }
 
+   /* open output1.txt file for reading */
+  test_file2 = fopen(filename2, "r");
+  if (test_file2 == NULL) {
+    printf("Could not open input file %s\n", filename2);
+    perror(filename2);
+    return(1);
+  }
+
+  /* For each character check if they are equal, if they aren't equal print error message and break out of the loop */
+  Char1 = fgetc(test_file1); /* get a character from the file */
+  Char2 = fgetc(test_file2);
+  while (Char1 != EOF || Char2 != EOF) {
+    if (Char1 != Char2){
+      printf("There is an error, the results aren't the same.");
+      break;
+    }
+    Char1 = fgetc(test_file1); /* get a character from the file */
+    Char2 = fgetc(test_file2);
+  }
+
+  printf("The results matched!\n");
+  
+  fclose(test_file1);
+  fclose(test_file2);
 }
