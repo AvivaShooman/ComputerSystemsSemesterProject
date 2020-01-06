0a42a91571f86ba09051ec569a35ee9dfcdecded
AvivaShooman <vavass18@gmail.com>
2020-01-05 21:20:01 -0500

    New file

---
+++ Final_Project_new.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX_LEN 10
+
+int main(void){
+  /* Declare all variables */
+  FILE *pipe_read, *pipe_transform, *test_file1, *test_file2;
+  char *filename1 = "./output1.txt";
+  char *filename2 = "./output2.txt";
+  char readbuf[80];
+  char *ans1, *ans2;
+  int pipe_test, number1, number2;
+
+  /* Create one way pipeline in with popen() that reads in the input file */
+  if ((pipe_read = popen("cat supermarket_sales.csv", "r")) == NULL){
+    perror("popen");
+    /* exit(1); */
+  }
+
+  /* Create one way pipeline out with popen() */
+  if ((pipe_transform = popen("grep -i 'Fashion accessories' | cut -d ',' -f 10| awk '{s+=$1} END {printf \"%0.2f\", s}'", "w")) == NULL){
+    perror("popen");
+    /* exit(1); */
+  }
+
+  /* Processing loop gets a line of input file, transforms it and prints to stdout*/
+  while(fgets(readbuf, 80, pipe_read)){
+    if (fputs(readbuf, pipe_transform) == EOF){
+      perror("fputs");
+      /*  exit(1); */
+	    
+    }
+  }
+	
+	
+  /* Close the pipes */
+  pclose(pipe_read);
+  pclose(pipe_transform);
+
+  /* Check that the pipe implemented above has the same output*/
+  if ((pipe_test = system("cat supermarket_sales.csv | grep -i 'Fashion accessories' | cut -d ',' -f 10| awk '{s+=$1} END {printf \"%0.2f\", s}'")) != -1){
+    perror("system");
+    /* exit(1); */
+  }
+
+}
